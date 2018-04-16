582b8258e03af5ab5bb25df70100a5e05285fec2
Taichi Tomioka <mys.itachi@gmail.com>
2018-04-16 15:52:33 +0900

    52

--- 52/D.cpp
+++ 52/D.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-	int N, A, B;
+	long N, A, B;
 
 	cin >> N >> A >> B;
 
-	vector<int> X(N);
+	vector<long> X(N);
 	for (int i = 0; i < N; i++) cin >> X[i];
 
-	int sum = 0;
+	long sum = 0;
 	for (int i = 1; i < N; i++) {
 		sum += min((X[i] - X[i - 1]) * A, B);
 	}
